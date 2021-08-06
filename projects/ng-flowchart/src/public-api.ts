460f28ac7c591cfb2cc395fedc0dac5947dc5b47
joel-wenzel <wenzje07@gmail.com>
2021-08-05 19:13:48 -0500

    explicitly adding entryComponents and exporting ngflow arrow

--- projects/ng-flowchart/src/public-api.ts
+++ projects/ng-flowchart/src/public-api.ts
@@ -7,5 +7,7 @@ export * from './lib/ng-flowchart-canvas.directive';
 export * from './lib/ng-flowchart-step-registry.service';
 export * from './lib/ng-flowchart-step.directive';
 export * from './lib/ng-flowchart-step/ng-flowchart-step.component';
+export * from './lib/ng-flowchart-arrow/ng-flowchart-arrow.component';
 export * from './lib/ng-flowchart.module';
+
 
